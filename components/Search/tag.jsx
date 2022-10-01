f9f4dc31a578f413c2d231f72431175637b3107b
rrramble <ru7ar7@gmail.com>
2022-10-02 00:01:07 +0500

    feature: save searched text into localStorage

--- components/Search/tag.jsx
+++ components/Search/tag.jsx
@@ -2,13 +2,14 @@ import Link from 'next/link'
 
 import styles from './tag.module.scss'
 
-export default function Tag({ link, text }) {
+export default function Tag({ link, text, cb }) {
   return (
     <Link
       href={link}
     >
       <a
         className={styles.text}
+        onClick={cb}
       >
         {text}
       </a>
