177fab575cd195d6ae3b53632da38b0878059cff
rrramble <ru7ar7@gmail.com>
2022-10-16 01:24:36 +0500

    feature: test '/' url for basic appearance

--- tests-cypress/e2e/index/1.appearance.cy.js
+++ tests-cypress/e2e/index/1.appearance.cy.js
@@ -1,30 +1,51 @@
+const { assert } = require("console")
+
 describe('Index page "/"', () => {
     beforeEach(() => {
       cy.visit('/')
     })
-  
+// TODO: change "[class]" to "[data-]"" attribute
     it('Checks menu icons: Logotype, Search, Favorites, History)', () => {
-      cy.get('header nav ul li[class^="header_logo"]').find('a').find('img:visible').eq(0).should('have.attr', 'alt', 'Логотип')
-      cy.get('header nav ul li[class^="header_search"]').find('button').eq(0).should('not.be.visible').should('have.text', 'Поиск')
-      cy.get('header nav ul li[class^="header_favorite"]').find('a').eq(0).should('have.attr', 'href', '/favorite').should('have.text', 'Избранное')
-      cy.get('header nav ul li[class^="header_history"]').find('button').eq(0).should('have.text', 'История поиска')
+      cy.get('header nav ul > li[class^="header_logo"]').find('a').find('img:visible').eq(0).should('have.attr', 'alt', 'Логотип')
+      cy.get('header nav ul > li[class^="header_search"]').find('button').eq(0).should('not.be.visible').should('have.text', 'Поиск')
+      cy.get('header nav ul > li[class^="header_favorite"]').find('a').eq(0).should('have.attr', 'href', '/favorite').should('have.text', 'Избранное')
+      cy.get('header nav ul > li[class^="header_history"]').find('button').eq(0).should('have.text', 'История поиска')
     })
-    /*
+
     it('Checks Images', () => {
-      const imageCards = cy.get('main').get('ul[class^="image-cards"]').get('li')
-      
-      imageCards.should('be.at.least', 3)
-      imageCards.should(lis => lis.forEach(checkImageCard))
-
-    })*/
+      cy.get('main ul[class^="image-cards"] > li').
+        each(($imageCard, index) => {
+          if (index > 5) {
+            return false
+          }
+          checkImageCard($imageCard)
+        })
+    })
   })
-  
-  function checkImageCard(li) {
-    li.get('img').should('exist')
 
-    // Local menu
-    const menuItems = li.find('menu li')
-    menuItems.should('have.length', 3)
+function checkImageCard($imageCard) {
+    cy.wrap($imageCard).find('img[class^="image-card_image"]:visible').
+      should('have.length', 1).
+      should('have.attr', 'src').
+      and('match', /^https:\/\/images.unsplash.com\/photo-/)
 
-    menuItems.eq(0).find('input[type="checkbox"]').should('have.text', '')
+    $imageCard.find('menu > li[class^="image-card-menu_"]').map((index, i) => {
+      if (index === 0) checkLikeButton(i)
+      if (index === 1) checkOpenButton(i)
+      if (index === 2) checkDownloadButton(i)
+    })
   }
+
+function checkLikeButton($el) {
+  cy.wrap($el).find('input')
+}
+
+function checkOpenButton($el) {
+  cy.wrap($el).find('a').
+    should('have.attr', 'href').
+    and('match', /^\/photo\//, { matchCase: false })
+}
+
+function checkDownloadButton($el) {
+  cy.wrap($el).find('button')
+}
