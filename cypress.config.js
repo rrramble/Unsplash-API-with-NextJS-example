7a85b5c262545ab13d400a6edd8e732baf0248dd
rrramble <ru7ar7@gmail.com>
2022-10-15 16:43:02 +0500

    refactor: add Cypress tesging framework

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  downloadsFolder: "tests-cypress/downloads",
+  fileServerFolder: "tests-cypress",
+  fixturesFolder: "tests-cypress/fixtures",
+  screenshotsFolder: "tests-cypress/snapshots/actual",
+  videosFolder: "tests-cypress/videos",
+
+  trashAssetsBeforeRuns: true,
+
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // node event listeners
+    },
+    supportFile: "tests-cypress/support/e2e.{js,jsx,ts,tsx}",
+    specPattern: "tests-cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+  },
+
+  component: {
+    supportFile: "tests-cypress/support/component.js"
+  },
+});
---
+++ tests-cypress/support/e2e.js
@@ -0,0 +1,15 @@
+// ***********************************************************
+// This file is processed and loaded automatically before the test files.
+// It stores global configuration and behavior that modifies Cypress.
+//
+// The 'supportFile' configuration option changes the location
+// of this file or turn off automatically serving support files
+//
+// More info here: https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
