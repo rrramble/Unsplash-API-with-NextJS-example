f59f177f4bba6221e28161975593b2a319adf198
rrramble <ru7ar7@gmail.com>
2022-10-16 20:58:29 +0500

    style: remove indentation and LF

--- components/header-logo.jsx
+++ components/header-logo.jsx
@@ -1,16 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-
 export default function HeaderLogo({ style }) {
   return (
-      <Link href="/">
-        <a className={style}>
-          <Image
-            src="/logo-white-on-black.svg"
-            alt="Логотип" width="32" height="32"
-          />
-        </a>
-      </Link>
+    <Link href="/">
+      <a className={style}>
+        <Image
+          src="/logo-white-on-black.svg"
+          alt="Логотип" width="32" height="32"
+        />
+      </a>
+    </Link>
   )
 }
