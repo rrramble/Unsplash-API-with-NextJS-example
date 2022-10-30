22c5ae6b6db2c5be75ce8c70c3b413b987d48699
rrramble <ru7ar7@gmail.com>
2022-10-31 00:50:18 +0500

    style: use '.self' instead of '.main' for main block

--- components/ImageCards/image-card.jsx
+++ components/ImageCards/image-card.jsx
@@ -21,7 +21,7 @@ export default function ImageCard({ photo, isLiked, onClickLikeButton }) {
   const photoProfileUrl = '/photo/' + (photoId ?? '')
 
   return (
-    <figure className={styles.main} data-test="image-card">
+    <figure className={styles.self} data-test="image-card">
       <figcaption className={styles['author-menu-container']}>
         <ImageCardAuthor
           name={author.name}
