296c01db135f79902ddf48aa8c23d146421117e8
rrramble <ru7ar7@gmail.com>
2023-02-02 16:16:21 +0600

    refactor: add isLazy for image

--- components/ImageCards/image-cards.jsx
+++ components/ImageCards/image-cards.jsx
@@ -4,13 +4,14 @@ import styles from './image-cards.module.scss'
 export default function ImageCards({ photos, likedPhotosIds = [], onClickLikeButton }) {
   return (
     <ul className={styles.self}>
-      { photos.map(photo =>
+      { photos.map((photo, index) =>
           <li
             className={styles.li}
             key={photo.id}
           >
             <ImageCard
               photo={photo}
+              isLasy={index <= 3}
               isLiked={likedPhotosIds.includes(photo.id)}
               onClickLikeButton={() => onClickLikeButton && onClickLikeButton(photo.id)}
             />
