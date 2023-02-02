366e018c55a6d5a7664bed0b2e374c2f68539dfd
rrramble <ru7ar7@gmail.com>
2023-02-02 15:54:02 +0600

    style: delete unused JS code

--- components/PhotosLayoutButtons/layout-buttons.jsx
+++ components/PhotosLayoutButtons/layout-buttons.jsx
@@ -1,25 +1,8 @@
-import { useState } from 'react'
-
 import styles from './layout-buttons.module.scss'
 import styles_line from './layout-line-button.module.scss'
 import styles_grid from './layout-grid-button.module.scss'
 
-const options = [
-  { id: 1, checked: true },
-  { id: 2, checked: false },
-];
-
-function getNewState(llist, newId) {
-  const newList = llist.map(item => {
-    return item.id === newId ?
-      { id: item.id, checked: true } :
-      { id: item.id, checked: false }
-    })
-  return newList
-}
-
 export default function LayoutButtons() {
-  const [ llist, setState ] = useState(options);
   return (
     <fieldset className={styles.self}>
       <legend className="visually-hidden">Раскладка фотографий:</legend>
@@ -29,7 +12,6 @@ export default function LayoutButtons() {
             type="radio"
             className={styles_line.self}
             name="photos-layout-buttons"
-            onChange={() => setState(getNewState(llist, 1))}
             defaultChecked
           />
           <span className="visually-hidden">Горизонтально</span>
@@ -42,7 +24,6 @@ export default function LayoutButtons() {
             type="radio"
             className={styles_grid.self}
             name="photos-layout-buttons"
-            onChange={() => setState(getNewState(llist, 2))}
           />
             <span className="visually-hidden">В квадратик</span>
         </label>
