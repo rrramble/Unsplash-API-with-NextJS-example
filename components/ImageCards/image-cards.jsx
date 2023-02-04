efd4772d660d2dbf54a37a0aa4cef97f1752292a
rrramble <ru7ar7@gmail.com>
2023-02-04 13:23:38 +0600

    feature: prefetch photo if its index <= 2

--- components/ImageCards/image-cards.jsx
+++ components/ImageCards/image-cards.jsx
@@ -8,7 +8,7 @@ const IMAGE_BOTTOM_MARGIN = 26
 export default function ImageCards({ photos, likedPhotosIds = [], onClickLikeButton }) {
   return (
     <ul className={styles.self}>
-      { photos.map(photo => {
+      { photos.map((photo, index) => {
           const { height, width } = photo
           const rowSpanCount = Math.floor((height / width * RENDERED_WIDTH + IMAGE_BOTTOM_MARGIN) / ROW_HEIGHT + 1)
 
@@ -17,9 +17,10 @@ export default function ImageCards({ photos, likedPhotosIds = [], onClickLikeBut
             style={{gridRow: 'span ' + rowSpanCount}}
           >
             <ImageCard
-              photo={photo}
               isLiked={likedPhotosIds.includes(photo.id)}
+              isPrefetched={index <= 2}
               onClickLikeButton={() => onClickLikeButton && onClickLikeButton(photo.id)}
+              photo={photo}
             />
           </li>
       })}
