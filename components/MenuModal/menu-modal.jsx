d62fa7cab6f07333d1dc97af6b0ff65ecb9959dc
rrramble <ru7ar7@gmail.com>
2023-02-04 15:37:28 +0600

    refactor: split additional classNames calculation

--- components/MenuModal/menu-modal.jsx
+++ components/MenuModal/menu-modal.jsx
@@ -1,15 +1,15 @@
 import styles from './menu-modal.module.scss'
 
 export default function MenuModal({ children, isHidden, isFullHeight }) {
-  const classNames =
-    styles.self + ' ' +
-    (isHidden ? styles['self--hidden'] :
-      isFullHeight ? styles['self--fill-window'] : ''
-    )
+  const additionalClassNames = isHidden ?
+    styles['self--hidden'] :
+    isFullHeight ?
+      styles['self--fill-window'] :
+      ''
 
   return (
     <div
-      className={classNames}
+      className={styles.self + ' ' + additionalClassNames}
     >
       {children}
     </div>
