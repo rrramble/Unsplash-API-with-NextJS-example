bafbe204e5e1ef26505740d623c9a189501c5209
rrramble <rustam.arkharov@gmail.com>
2023-02-06 12:17:13 +0600

    feature: add 'show more' button

--- components/IndividualImageCard/similar-photos.tsx
+++ components/IndividualImageCard/similar-photos.tsx
@@ -11,11 +11,17 @@ export default function SimilarPhotos ({
     <div
       className={styles.self}
     >
-      <header
-        className={styles.header}
-      >
-        Похожие фотографии
-      </header>
+      <div className={styles.header}>
+        <header
+          className={styles.title}
+        >
+          Похожие фотографии
+        </header>
+        <button className={styles['show-more']}>
+          <span className="visually-hidden">Показать больше</span>
+        </button>
+      </div>
+
       <ImageCards
         photos={photos}
         likedPhotosIds={likedPhotosIds}
