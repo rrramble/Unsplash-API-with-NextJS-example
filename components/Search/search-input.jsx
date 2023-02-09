2150eb1df27f55d15e7de14820ee98c2ff7f4356
rrramble <rustam.arkharov@gmail.com>
2023-02-09 22:21:12 +0600

    fix: add dependency vars to useEffect

--- components/Search/search-input.jsx
+++ components/Search/search-input.jsx
@@ -5,7 +5,9 @@ import styles from './search-input.module.scss'
 export default function SearchInput({ style, passRef }) {
   const inputRef = useRef(null)
 
-  useEffect(() => passRef(inputRef))
+  useEffect(() => {
+    passRef && passRef(inputRef)
+  }, [passRef, inputRef])
 
   return (
     <div
