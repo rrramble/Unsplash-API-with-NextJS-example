4a8252d16dff8a5286099aea397ce7590b59d07a
rrramble <rustam.arkharov@gmail.com>
2023-02-16 00:04:37 +0600

    feature: add func fetching Photo JSON from Unsplash

---
+++ utils/unsplash.ts
@@ -0,0 +1,36 @@
+import { env } from 'node:process'
+
+function getPhotoJSONUrl(id: string) {
+  if (!env?.UNSPLASH_API_KEY) {
+    throw new Error(
+      'Error. Provide UNSPLASH_KEY in your NodeJS environment.'
+    )
+  }
+  return (
+    `https://api.unsplash.com/photos/${id}?client_id=${env.UNSPLASH_API_KEY}`
+  )
+}
+
+export async function fetchPhoto(id: string): Promise<string> {
+  const url = getPhotoJSONUrl(id)
+  let response: Response
+  try {
+    response = await fetch(url)
+    if (response.status !== 200) {
+      throw new Error(
+        `Error fetching status: ${response.status}.`
+      )
+    }
+  } catch (e) {
+    throw new Error(e.message)
+  }
+
+  try {
+    return await response.text()
+  } catch(e) {
+    throw new Error(
+      `Recieved response from Unsplash.com,
+      but could not get its data.`
+    )
+  }
+}
