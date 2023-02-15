c8c3d1298129409ab5fffcbef2a45ee2c1d57b73
rrramble <rustam.arkharov@gmail.com>
2023-02-15 10:34:18 +0600

    fix: allow using downloaded font due to NextJS bug

--- components/layout.jsx
+++ components/layout.jsx
@@ -11,7 +11,7 @@ export default function Layout({ children, topics }) {
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="Training project using API of the 'Unsplash.com' portal." />
-        <link rel="preload" href="/fonts/sf-ui-display-medium-58646be638f96.woff" as="font" />
+        <link rel="preload" href="/fonts/sf-ui-display-medium-58646be638f96.woff" as="font" crossOrigin="true" />
       </Head>
       <header
         className={styles.header}
