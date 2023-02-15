5bc7148533e036d53f60d5bc35fc41450baf45cf
rrramble <rustam.arkharov@gmail.com>
2023-02-16 00:06:19 +0600

    feature: fetch Photo JSON from Unsplash.com if no local file available

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,13 +1,62 @@
 import path from 'path'
 import { promises as fs } from 'fs'
+import { fetchPhoto } from './unsplash'
 
-async function getJSONDataFile(pathParts: string[]) {
-  const jsonFileFullPath = path.join(process.cwd(), 'data', ...pathParts)
+export async function getPhoto(id: string) {
+  try {
+    return await getJSONDataFile(['photos', id + '.json'])
+  } catch(e) {
+    console.info(e.message)
+  }
+
   let contents: string
   try {
-    contents = await fs.readFile(jsonFileFullPath, 'utf8')
+    contents = await fetchPhoto(id)
+  } catch (e) {
+    throw new Error(e.message)
+  }
+
+  let json
+  try {
+    json = JSON.parse(contents)
   } catch(e) {
-    contents = null
+    throw new Error(`Could not parse json with photo ID: ${id}`)
+  }
+
+  try {
+    saveFile(['photos', `${id}.json`], contents)
+  } catch(e) {
+    console.log(`Could not save photo info with ID: ${id}`)
+  }
+  return json
+}
+
+export async function getPhotos(slugName: string) {
+  return await getJSONDataFile(['topics', slugName + '.json'])
+}
+
+export async function getTopics() {
+  return await getJSONDataFile(['topics', '__items__.json'])
+}
+
+
+// Internal functions
+
+async function getFileContents(pathParts: string[]) {
+  const fileFullPath = path.join(process.cwd(), 'data', ...pathParts)
+  try {
+    return await fs.readFile(fileFullPath, 'utf8')
+  } catch(e) {
+    throw new Error(`Error reading file: "${fileFullPath}"`)
+  }
+}
+
+async function getJSONDataFile(pathParts: string[]) {
+  let contents: string
+  try {
+    contents = await getFileContents(pathParts)
+  } catch(e) {
+    throw new Error(e.message)
   }
 
   try {
@@ -17,14 +66,11 @@ async function getJSONDataFile(pathParts: string[]) {
   }
 }
 
-export async function getPhoto(id: string) {
-  return await getJSONDataFile(['photos', id + '.json'])
-}
-
-export async function getPhotos(slugName: string) {
-  return await getJSONDataFile(['topics', slugName + '.json'])
-}
-
-export async function getTopics() {
-  return await getJSONDataFile(['topics', '__items__.json'])
+async function saveFile(pathParts: string[], contents: string) {
+  const fileFullPath = path.join(process.cwd(), 'data', ...pathParts)
+  try {
+    await fs.writeFile(fileFullPath, contents)
+  } catch (e) {
+    throw new Error(`Could not save file: ${fileFullPath}`)
+  }
 }
