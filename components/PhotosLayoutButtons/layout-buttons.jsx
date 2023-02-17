d8f6543b04605f96f6a8dd397a58c2f2e3e56213
rrramble <rustam.arkharov@gmail.com>
2023-02-17 13:13:16 +0600

    refactor: change css-only icons to svg

--- components/PhotosLayoutButtons/layout-buttons.jsx
+++ components/PhotosLayoutButtons/layout-buttons.jsx
@@ -1,33 +1,27 @@
 import styles from './layout-buttons.module.scss'
-import styles_line from './layout-line-button.module.scss'
-import styles_grid from './layout-grid-button.module.scss'
 
 export default function LayoutButtons() {
   return (
     <fieldset className={styles.self}>
       <legend className="visually-hidden">Раскладка фотографий:</legend>
-      <div className={styles.item} data-test="images-layout-button">
-        <label>
+        <label className={styles.item} data-test="images-layout-button">
           <input
             type="radio"
-            className={styles_line.self}
+            className={styles['item--by-lines']}
             name="photos-layout-buttons"
             defaultChecked
           />
           <span className="visually-hidden">Горизонтально</span>
         </label>
-      </div>
 
-      <div className={styles.item} data-test="images-layout-button">
-        <label>
-          <input
-            type="radio"
-            className={styles_grid.self}
-            name="photos-layout-buttons"
-          />
-            <span className="visually-hidden">В квадратик</span>
-        </label>
-      </div>
+      <label className={styles.item} data-test="images-layout-button">
+        <input
+          type="radio"
+          className={styles['item--by-squares']}
+          name="photos-layout-buttons"
+        />
+          <span className="visually-hidden">В квадратик</span>
+      </label>
     </fieldset>
   )
 }
