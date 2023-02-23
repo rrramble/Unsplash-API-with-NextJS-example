a04458240df4f2bca04ab57b3e4e832581feb061
rrramble <rustam.arkharov@gmail.com>
2023-02-24 00:38:08 +0600

    style: add space

--- components/Search/search.jsx
+++ components/Search/search.jsx
@@ -18,7 +18,7 @@ export default function Search({
 }) {
 
   const formRef = useRef()
-  const [ inputRef, setInputRef] = useState()
+  const [ inputRef, setInputRef ] = useState()
 
   let additionalClassName = ''
   if (isHidden) {
