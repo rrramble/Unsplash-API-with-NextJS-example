56aa5b67898229044aa3bc5085558a3b82406afd
rrramble <rustam.arkharov@gmail.com>
2023-03-01 23:52:23 +0600

    refactor: remove unused prop 'topics' in SearchIcon

--- components/header.jsx
+++ components/header.jsx
@@ -62,7 +62,6 @@ export default function Header({ topics, isRootPage }) {
             dispatch({ type: 'search-icon-clicked' })
           }}
           state={state.searchIcon}
-          topics={topics}
         />
 
         <Favorite
