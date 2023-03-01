01a79be4b16ea6341276832a3f769e3e8ab5e214
rrramble <rustam.arkharov@gmail.com>
2023-03-01 23:52:59 +0600

    refactor: remove <meta> of viewport

--- components/layout.jsx
+++ components/layout.jsx
@@ -9,7 +9,6 @@ export default function Layout({ children, topics }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="Training project using API of the 'Unsplash.com' portal." />
         <link rel="preload" href="/fonts/sf-ui-display-medium-58646be638f96.woff" as="font" crossOrigin="true" />
       </Head>
