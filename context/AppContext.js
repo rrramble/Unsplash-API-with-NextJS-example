2e943a9924355ee0725dd7d444575ab3a4a85744
rrramble <rustam.arkharov@gmail.com>
2023-03-15 12:25:01 +0600

    fix: change destriction from {} to []

--- context/AppContext.js
+++ context/AppContext.js
@@ -4,10 +4,11 @@ import { AppReducer, initialState } from "./AppReducer";
 const AppContext = createContext();
 
 export function AppWrapper({ children }) {
-   const { state, dispatch } = useReducer(AppReducer, initialState);
+   const [ state, dispatch ] = useReducer(AppReducer, initialState)
+
    const contextValue = useMemo(() => {
-      return { state, dispatch };
-   }, [state, dispatch]);
+      return { state, dispatch }
+   }, [state, dispatch])
 
    return (
       <AppContext.Provider value={contextValue}>
