d2f008cf12215dda7ea74cfcc5b35549df0829fa
rrramble <rustam.arkharov@gmail.com>
2023-03-15 12:28:58 +0600

    feat: render layout buttons based on photo column count

--- components/image-cards/image-cards.jsx
+++ components/image-cards/image-cards.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react'
+import { useAppContext } from '@/context/AppContext'
 import ImageCard from './image-card'
 import styles from './image-cards.module.scss'
 
@@ -6,9 +8,29 @@ export default function ImageCards({
   onClickLikeButton,
   photos,
 }) {
+  const { dispatch } = useAppContext()
+  const layoutRef = useRef()
+
+  const handleWindowResize = () => {
+    const style = window.getComputedStyle(layoutRef.current, null)
+    const columnCountAsString = style['column-count'] || '0'
+    const columnCount = parseInt(columnCountAsString, 10)
+    dispatch({ type: 'SAVE_PHOTO_COLUMN_COUNT', payload: columnCount })
+  }
+
+  useEffect(() => {
+    handleWindowResize()
+    window.addEventListener('resize', handleWindowResize)
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize)
+    };
+  }, [])
+
   return (
     <ul
       className={styles.self}
+      ref={layoutRef}
     >
       { photos.map((photo, index) => {
         return (
@@ -27,4 +49,8 @@ export default function ImageCards({
       })}
     </ul>
   )
+}
+
+function getColumnCount(node) {
+
 }
--- components/layout-buttons/layout-buttons.jsx
+++ components/layout-buttons/layout-buttons.jsx
@@ -1,27 +1,34 @@
 import styles from './layout-buttons.module.scss'
+import { useAppContext } from '@/context/AppContext'
 
 export default function LayoutButtons() {
+  const { state: { photoColumnCount } } = useAppContext()
+
   return (
     <fieldset className={styles.self}>
       <legend className="visually-hidden">Раскладка фотографий:</legend>
-        <label className={styles.item} data-test="images-layout-button">
-          <input
-            type="radio"
-            className={styles['item--by-lines']}
-            name="photos-layout-buttons"
-            defaultChecked
-          />
-          <span className="visually-hidden">Горизонтально</span>
-        </label>
-
       <label className={styles.item} data-test="images-layout-button">
         <input
-          type="radio"
-          className={styles['item--by-squares']}
+          checked={photoColumnCount <= 1}
+          className={styles['item--by-lines']}
+          defaultChecked
+          disabled
           name="photos-layout-buttons"
+          type="radio"
         />
-          <span className="visually-hidden">В квадратик</span>
+        <span className="visually-hidden">Горизонтально</span>
       </label>
+
+    <label className={styles.item} data-test="images-layout-button">
+      <input
+        checked={photoColumnCount >= 2}
+        className={styles['item--by-squares']}
+        disabled
+        name="photos-layout-buttons"
+        type="radio"
+      />
+        <span className="visually-hidden">В квадратик</span>
+    </label>
     </fieldset>
   )
 }
