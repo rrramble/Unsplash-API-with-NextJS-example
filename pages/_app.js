a5e811901fe54a74dcc5641b2ffddfb06678027b
rrramble <rustam.arkharov@gmail.com>
2023-03-29 17:52:46 +0600

    refactor: use @styles, @components instead of realtive

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import '../styles/globals.scss'
+import '@styles/globals.scss'
 
-import Layout from '../components/layout'
+import Layout from '@/components/layout'
 import { AppWrapper } from "@/context/AppContext";
 
 function MyApp({ Component, pageProps }) {
