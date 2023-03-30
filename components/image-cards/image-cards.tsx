2d27403de01217560c72a571190339b506cc7973
rrramble <rustam.arkharov@gmail.com>
2023-03-31 01:11:48 +0600

    refactor: extract useWindowResize to 'hooks/...'

--- components/image-cards/image-cards.tsx
+++ components/image-cards/image-cards.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef } from 'react'
-import { useAppContext } from '@/context/AppContext'
-import { throttle } from '@/utils/helper-browser'
+import { useRef } from 'react'
 import ImageCard from './image-card'
 import styles from './image-cards.module.scss'
 import { PhotoIds, PhotoId, Photos } from 'types/photos'
+import { useWindowResize } from 'hooks/use-window-resize'
+import { WINDOW_RESIZE_THROTTLE_DELAY_MS } from 'consts/consts'
 
 type ImageCardsProps = {
   likedPhotosIds: PhotoIds,
@@ -16,24 +16,9 @@ export default function ImageCards({
   onClickLikeButton,
   photos,
 }: ImageCardsProps) {
-  const { dispatch } = useAppContext()
   const layoutRef = useRef()
 
-  useEffect(() => {
-    const handleWindowResize = throttle(() => {
-      const style = window.getComputedStyle(layoutRef.current, null)
-      const columnCountAsString = style['column-count'] || '0'
-      const columnCount = parseInt(columnCountAsString, 10)
-      dispatch({ type: 'SAVE_PHOTO_COLUMN_COUNT', payload: columnCount })
-    }, 100)
-
-    handleWindowResize()
-    window.addEventListener('resize', handleWindowResize)
-
-    return () => {
-      window.removeEventListener('resize', handleWindowResize)
-    };
-  }, [dispatch])
+  useWindowResize(layoutRef, WINDOW_RESIZE_THROTTLE_DELAY_MS)
 
   return (
     <ul
---
+++ hooks/use-window-resize.ts
@@ -0,0 +1,29 @@
+import { MutableRefObject, useEffect } from 'react'
+import { useAppContext } from '@/context/AppContext'
+import { throttle } from '@/utils/helper-browser'
+
+export function useWindowResize(
+    layoutRef: MutableRefObject<HTMLElement | null>,
+    timeoutMs: number
+) {
+  const { dispatch } = useAppContext()
+
+  useEffect(() => {
+    const handleWindowResize = throttle(() => {
+      if (!layoutRef.current) {
+        return
+      }
+      const style = window.getComputedStyle(layoutRef.current, null)
+      const columnCountAsString = style['column-count'] || '0'
+      const columnCount = parseInt(columnCountAsString, 10)
+      dispatch({ type: 'SAVE_PHOTO_COLUMN_COUNT', payload: columnCount })
+    }, timeoutMs)
+
+    handleWindowResize()
+    window.addEventListener('resize', handleWindowResize)
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize)
+    };
+  }, [dispatch, layoutRef, timeoutMs])
+}
