06718d934019599b21b5f7251cf8c97c9597747e
rrramble <rustam.arkharov@gmail.com>
2023-03-30 09:56:18 +0600

    refactor: make TSX from JSX

--- components/history/tags.tsx
+++ components/history/tags.tsx
@@ -1,7 +1,12 @@
+import { HistoryEntry } from 'types/history'
 import Tag from './tag'
 import styles from './tags.module.scss'
 
-export default function Tags({ items }) {
+type TagsProps = {
+  items: HistoryEntry[] | null,
+}
+
+export default function Tags({ items }: TagsProps) {
   if (!items || !items.length) {
     return null
   }
