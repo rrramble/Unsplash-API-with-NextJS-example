1bf78bf00dcba403e52de99beaef67f45c2bd68d
rrramble <rustam.arkharov@gmail.com>
2023-03-31 00:11:55 +0600

    feat: add types for Photo object

--- types/photos.ts
+++ types/photos.ts
@@ -1,12 +1,32 @@
+type PhotoUrls = {
+  raw?: string,
+  full?: string,
+  regular?: string,
+  small?: string,
+  thumb?: string,
+}
+
+type ProfileUrl = {
+  large?: string,
+  medium? : string,
+  small? : string,
+}
+
 type User = {
+  instagram_username: string | null,
   name: string,
-  instagragm?: string,
+  profile_image: ProfileUrl,
 }
 
 export type Photo = {
-  description: string,
+  alt_description?: string,
+  color?: number,
+  description?: string,
+  height: number,
   id: string,
+  urls: PhotoUrls,
   user: User,
+  width: number,
 }
 
 export type Photos = Photo[]
