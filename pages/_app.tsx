186e75fe68988d7639e38841dd1f15fb6a9e11a7
rrramble <rustam.arkharov@gmail.com>
2023-03-31 20:21:14 +0600

    refactor: move <Layout...> files to 'components/layout'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@styles/globals.scss'
 import { AppProps } from 'next/app'
-import Layout from '@/components/layout'
+import Layout from '@/components/layout/layout'
 import { AppWrapper } from "@/context/app-context";
 
 function MyApp({ Component, pageProps }: AppProps) {
