2d39b58da7c088947a755f7246e42490315d4689
rrramble <rustam.arkharov@gmail.com>
2023-03-31 21:59:07 +0600

    feat: show button if scrollY > 1

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,3 +1,4 @@
+import { useWindowScrollY } from 'hooks/use-window-scroll-y'
 import { PlainFunction } from 'types/types'
 import styles from './footer.module.scss'
 
@@ -6,9 +7,14 @@ type FooterProps = {
 }
 
 export default function Footer({ onClick }: FooterProps) {
+  const scrollY = useWindowScrollY(300)
+  const buttonAdditionalClassName = scrollY === 0 ?
+    styles['self--hidden'] :
+    ''
+
   return (
     <button
-      className={styles['go-up-button']}
+      className={`${styles.self} ${buttonAdditionalClassName}`}
       onClick={onClick}
     >
       <span className='visually-hidden'>
