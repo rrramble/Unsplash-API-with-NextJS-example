f2954510e296d230ae74623c4810564675d218b3
rrramble <rustam.arkharov@gmail.com>
2023-04-03 00:39:21 +0600

    refactor: rename type to 'AppState' from 'State'

--- hooks/store.ts
+++ hooks/store.ts
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import { TypedUseSelectorHook, useDispatch } from 'react-redux';
-import { AppDispatch, State } from 'types/state';
+import { AppDispatch, AppState } from 'types/state';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
+export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
