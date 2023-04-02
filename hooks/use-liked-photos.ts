6adaf50a17057605655f6b0f0022406c22aef000
rrramble <rustam.arkharov@gmail.com>
2023-04-02 22:13:29 +0600

    refactor: read store, instead of localStorage

--- hooks/use-liked-photos.ts
+++ hooks/use-liked-photos.ts
@@ -1,19 +1,10 @@
-import { useEffect, useState } from 'react'
-import { Photos } from 'types/photos'
 import { useAppSelector } from 'hooks/store'
-import { useLikedPhotoIds } from './use-liked-photo-ids'
 
 export function useLikedPhotos() {
   const allPhotos = useAppSelector(store => store.photos)
-  const likedPhotoIds = useLikedPhotoIds()
-  const [ likedPhotos, setLikedPhotos ] = useState<Photos>([])
+  const likedPhotoIds = useAppSelector(store => store.favoritePhotoIds)
 
-  useEffect(() => {
-    const filtered = allPhotos.filter(
-        ({ id }) => likedPhotoIds.includes(id)
-    )
-    setLikedPhotos(filtered)
-  }, [likedPhotoIds, allPhotos])
-
-  return likedPhotos
+  return allPhotos.filter(
+      photo => likedPhotoIds.includes(photo.id)
+  )
 }
