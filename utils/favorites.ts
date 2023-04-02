551ad87b511dfa2927804b866aa1d99e9410d942
rrramble <rustam.arkharov@gmail.com>
2023-04-03 01:07:23 +0600

    refactor: remove unused imports

--- utils/favorites.ts
+++ utils/favorites.ts
@@ -1,9 +1,5 @@
-import { store } from 'store'
-import { addPhoto } from 'store/actions'
-import { lsAddItem, lsGetArray, lsRemoveItem, subscribeOnChange } from 'utils/local-storage'
-import { fetchPhoto } from 'utils/helper-browser'
+import { lsAddItem, lsGetArray, lsRemoveItem } from 'utils/local-storage'
 import { PhotoId, PhotoIds } from 'types/photos'
-import { PlainFunction } from 'types/types'
 
 export function getFavoritePhotosIds(): PhotoIds {
   return lsGetArray<PhotoId>('favoritePhotosIds')
