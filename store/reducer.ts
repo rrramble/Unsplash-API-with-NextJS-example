8f15db58080103a3a0a74125c45ffc68043943a5
rrramble <rustam.arkharov@gmail.com>
2023-04-02 14:12:28 +0600

    feat: add cases to 'setPhotos' and 'addPhoto'

--- store/reducer.ts
+++ store/reducer.ts
@@ -1,20 +1,31 @@
 import { createReducer } from '@reduxjs/toolkit'
-import { saveColumnCount, setPhotos } from 'store/actions'
-import { Photos } from 'types/photos';
+import { setColumnCount } from 'store/actions'
+import { addPhoto, setPhotos } from 'store/actions'
+import { Photos } from 'types/photos'
 
 const initialState = {
   columnCount: 0,
   photos: <Photos>[],
 }
 
-export const reducer = createReducer(initialState, builder => {
-  builder
-    .addCase(saveColumnCount, (state, { payload }) => {
-      if (state.columnCount !== payload) {
-        state.columnCount = payload
-      }
-    })
-    .addCase(setPhotos, (state, { payload }) => {
-      state.photos = payload
-    })
-})
+export const reducer = createReducer(
+    initialState,
+    builder => {
+      builder
+        .addCase(setColumnCount, (state, { payload }) => {
+          if (state.columnCount !== payload) {
+            state.columnCount = payload
+          }
+        })
+        .addCase(setPhotos, (state, { payload }) => {
+          state.photos = payload
+        })
+        .addCase(addPhoto, (state, { payload }) => {
+          // TODO: check if outtdated
+          if (state.photos.some(photo => photo.id === payload.id)) {
+            return
+          }
+          state.photos.push(payload)
+        })
+    }
+)
