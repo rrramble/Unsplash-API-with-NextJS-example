3a7af0bf5735a600db6412cc015a58bfc8771230
rrramble <rustam.arkharov@gmail.com>
2023-04-03 00:35:02 +0600

    fix: add useEffect() to hook

--- hooks/use-liked-photos.ts
+++ hooks/use-liked-photos.ts
@@ -1,10 +1,19 @@
+import { useEffect, useState } from 'react'
 import { useAppSelector } from 'hooks/store'
+import { Photos } from 'types/photos'
 
 export function useLikedPhotos() {
   const allPhotos = useAppSelector(store => store.photos)
   const likedPhotoIds = useAppSelector(store => store.favoritePhotoIds)
 
-  return allPhotos.filter(
-      photo => likedPhotoIds.includes(photo.id)
-  )
+  const [ likedPhotos, setLikedPhotos ] = useState<Photos>([])
+
+  useEffect(() => {
+    const resultPhotos = allPhotos.filter(
+        photo => likedPhotoIds.includes(photo.id)
+    )
+    setLikedPhotos(resultPhotos)
+  }, [allPhotos, likedPhotoIds])
+
+  return likedPhotos
 }
