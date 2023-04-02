0d7dd79575e6374913c1bf62853a15ab757898a8
rrramble <rustam.arkharov@gmail.com>
2023-04-02 20:20:54 +0600

    feat: add action creators for favorite photos

--- store/actions.ts
+++ store/actions.ts
@@ -1,7 +1,10 @@
 import { createAction } from '@reduxjs/toolkit'
-import { Photo, Photos } from 'types/photos'
+import { Photo, PhotoId, PhotoIds, Photos } from 'types/photos'
 
 export const setColumnCount = createAction<number>('columnCount/set')
 
 export const addPhoto = createAction<Photo>('photos/addOne')
 export const setPhotos = createAction<Photos>('photos/set')
+
+export const setFavoriteIds = createAction<PhotoIds>('favorites/set')
+export const toggleOneFavoriteId = createAction<PhotoId>('favorites/toggleOne')
