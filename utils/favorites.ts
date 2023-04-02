5c41c9872de2add473d403dc8df91c605436ceee
rrramble <rustam.arkharov@gmail.com>
2023-04-03 01:03:35 +0600

    refactor: remove unused func

--- utils/favorites.ts
+++ utils/favorites.ts
@@ -9,37 +9,10 @@ export function getFavoritePhotosIds(): PhotoIds {
   return lsGetArray<PhotoId>('favoritePhotosIds')
 }
 
-export function isPhotoIdLiked(id: PhotoId) {
-  return getFavoritePhotosIds().includes(id)
-}
-
 export function saveFavoritePhotoId(id: PhotoId) {
   return lsAddItem('favoritePhotosIds', id)
 }
 
 export function removeFavoritePhotoId(id: PhotoId) {
   return lsRemoveItem('favoritePhotosIds', id, id => id)
-}
-
-export function subscribeOnChangeFavorites(cb: PlainFunction) {
-  subscribeOnChange('favoritePhotosIds', cb)
-}
-
-export function toggleFavoriteStatus(id: PhotoId) {
-  const likedPhotosIds = getFavoritePhotosIds()
-  if (likedPhotosIds.includes(id)) {
-    removeFavoritePhotoId(id)
-    return
-  }
-
-  saveFavoritePhotoId(id)
-  const { photos } = store.getState()
-  const isExistInState = photos.some(photo => photo.id === id)
-  if (isExistInState) {
-    return
-  }
-
-  fetchPhoto(id)
-    .then(photo => store.dispatch(addPhoto(photo)))
-    .catch()
 }
