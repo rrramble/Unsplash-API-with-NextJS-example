2fbfe4dc682257db29b8cbce8b61e41d3c1e0a41
rrramble <rustam.arkharov@gmail.com>
2023-05-04 08:41:33 +0600

    feat: add 'getPhotosSelector()' state selector

--- store/selectors.ts
+++ store/selectors.ts
@@ -1,6 +1,10 @@
-import { PhotoIds } from 'types/photos';
+import { PhotoIds, Photos } from 'types/photos';
 import { AppState } from 'types/state';
 
 export function getFavoritePhotoIdsSelector(state: AppState): PhotoIds {
   return state.favoritePhotoIds;
+}
+
+export function getPhotosSelector(state: AppState): Photos {
+  return state.photos;
 }
