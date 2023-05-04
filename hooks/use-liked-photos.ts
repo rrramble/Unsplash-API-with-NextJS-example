5c4f8205a2e816fab6fb63b87ce3de493e92edd8
rrramble <rustam.arkharov@gmail.com>
2023-05-04 08:41:59 +0600

    refactor: use state selectors

--- hooks/use-liked-photos.ts
+++ hooks/use-liked-photos.ts
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react'
 import { useAppSelector } from 'hooks/store'
+import { getFavoritePhotoIdsSelector, getPhotosSelector } from 'store/selectors'
 import { Photos } from 'types/photos'
 
 export function useLikedPhotos() {
-  const allPhotos = useAppSelector(store => store.photos)
-  const likedPhotoIds = useAppSelector(store => store.favoritePhotoIds)
-
+  const allPhotos = useAppSelector(getPhotosSelector)
+  const likedPhotoIds = useAppSelector(getFavoritePhotoIdsSelector)
   const [ likedPhotos, setLikedPhotos ] = useState<Photos>([])
 
   useEffect(() => {
@@ -13,7 +13,7 @@ export function useLikedPhotos() {
         photo => likedPhotoIds.includes(photo.id)
     )
     setLikedPhotos(resultPhotos)
-  }, [allPhotos, likedPhotoIds])
+  }, [ allPhotos, likedPhotoIds ])
 
   return likedPhotos
 }
