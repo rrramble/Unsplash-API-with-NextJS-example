be33e82a1091839ddd44566745ae8c0d5086f49f
rrramble <rustam.arkharov@gmail.com>
2023-05-07 17:45:55 +0600

    feat: add component <NavButton>

---
+++ components/nav-button/nav-button.tsx
@@ -0,0 +1,33 @@
+import { MouseEventHandler } from 'react'
+import styles from './nav-button.module.scss'
+
+type NavButtonProps = {
+  className: string,
+  dataTestId?: string,
+  isHidden: boolean,
+  onClick: MouseEventHandler<HTMLButtonElement>,
+  title: string,
+  wrapperClassName: string,
+}
+
+export default function NavButton(props: NavButtonProps): JSX.Element {
+  return (
+    <div
+      className={props.wrapperClassName}
+    >
+      <button
+        aria-haspopup="true"
+        className={`${styles.self} ${props.className} ${props.isHidden ? styles['self--hidden'] : ''}`}
+        data-testid={props.dataTestId}
+        onClick={props.onClick}
+        type="button"
+      >
+        <span
+          className={styles.title}
+        >
+          {props.title}
+        </span>
+      </button>
+    </div>
+  )
+}
