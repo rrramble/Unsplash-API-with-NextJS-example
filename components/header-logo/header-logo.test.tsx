f4940204031293d01b63dc868e9d58124a736422
rrramble <rustam.arkharov@gmail.com>
2023-05-07 14:12:50 +0600

    test: add of <HeaderLogo> component

---
+++ components/header-logo/header-logo.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/react'
+import "@testing-library/jest-dom";
+import HeaderLogo from './header-logo'
+import { AppRoute } from 'consts/consts';
+
+
+describe('Component: <HeaderLogo>', () => {
+  it('renders component on the root page', () => {
+    render(
+        <HeaderLogo
+          className={'FAKE_CLASS_NAME'}
+          isRootPage={true}
+        />
+    )
+
+    expect(screen.queryByRole('link'))
+      .not.toBeInTheDocument()
+
+    expect(screen.getByRole('img'))
+      .toBeInTheDocument()
+  })
+
+
+  it('renders component not on the root page', () => {
+    render(
+        <HeaderLogo
+          className={'FAKE_CLASS_NAME'}
+          isRootPage={false}
+        />
+    )
+
+    expect(screen.getByRole('link'))
+      .toHaveAttribute('href', AppRoute.Root)
+
+    expect(screen.getByRole('img'))
+      .toBeInTheDocument()
+  })
+})
