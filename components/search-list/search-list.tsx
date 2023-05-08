fc3502727b45dec66fb7cc4680957d026a23569f
rrramble <rustam.arkharov@gmail.com>
2023-05-08 13:03:57 +0600

    style: rename component and props type <SearchList>

--- components/search-list/search-list.tsx
+++ components/search-list/search-list.tsx
@@ -3,13 +3,13 @@ import { AppRoute } from 'consts/consts'
 import { SearchTopic, SearchTopics } from 'types/search-tags'
 import styles from './search-list.module.scss'
 
-type TagsProps = {
+type SearchListProps = {
   isFull: boolean,
   items: SearchTopics,
   onClick: (_tag: SearchTopic) => void,
 }
 
-export default function Tags({ items, isFull, onClick }: TagsProps) {
+export default function SearchList({ items, isFull, onClick }: SearchListProps) {
   const additionalClassName = isFull ?
     styles['self--full'] :
     styles['self--minimized']
