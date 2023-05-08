f6e97c71e45ef3a7972110db7552019d3643eac9
rrramble <rustam.arkharov@gmail.com>
2023-05-08 11:04:52 +0600

    refactor: hide <Footer> with React/JS

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,20 +1,27 @@
 import { useWindowScrollY } from 'hooks/use-window-scroll-y'
 import { PlainFunction } from 'types/types'
 import styles from './footer.module.scss'
+import { useEffect, useState } from 'react'
 
 type FooterProps = {
   onClick: PlainFunction,
 }
 
-export default function Footer({ onClick }: FooterProps) {
-  const scrollY = useWindowScrollY(300)
-  const buttonAdditionalClassName = scrollY === 0 ?
-    styles['self--hidden'] :
-    ''
+export default function Footer({ onClick }: FooterProps): JSX.Element {
+  const scrollY = useWindowScrollY(0)
+  const [ isHidden, setIsHidden ] = useState<boolean>(true)
+
+  useEffect(()  => {
+    setIsHidden(scrollY === 0)
+  }, [ scrollY ])
+
+  if (isHidden) {
+    return null
+  }
 
   return (
     <button
-      className={`${styles.self} ${buttonAdditionalClassName}`}
+      className={styles.self}
       onClick={onClick}
     >
       <span className='visually-hidden'>
