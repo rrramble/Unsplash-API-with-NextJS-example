e04c8b7583cf58ec55b70a55796b08b3cf87e7d5
rrramble <rustam.arkharov@gmail.com>
2023-05-09 17:01:09 +0600

    test: add of '<HistoryList>' component

---
+++ components/history-list/history-list.test.tsx
@@ -0,0 +1,56 @@
+import { cleanup, render, screen } from '@testing-library/react'
+import { makeMockHistoryEntries } from '@/utils/mocks'
+import HistoryList from './history-list'
+import { AppRoute } from 'consts/consts'
+import { HistoryEntries } from 'types/history'
+
+const MockItemsCount = [ 0, 1, 100 ]
+
+describe('Component: <SearchList />', () => {
+  beforeEach(() => {
+    cleanup()
+  })
+
+  test.concurrent.each(MockItemsCount)('renders correctly with items count: %s', (mockItemsCount) => {
+    cleanup()
+    const mockItems = makeMockHistoryEntries(mockItemsCount)
+
+    render(
+        <HistoryList
+          items={mockItems}
+        />
+    )
+
+    const foundLinks = screen.queryAllByRole('link')
+
+    expect(foundLinks.length)
+      .toEqual(mockItemsCount)
+
+    foundLinks.forEach((foundLink, index) => {
+      const href = foundLink.getAttribute('href')
+      const { slug, title } = mockItems[index]
+      if (slug === undefined) console.log('title: ', title)
+      const hrefToBe = slug === undefined ?
+        `${AppRoute.Search}/${encodeURIComponent(title)}` :
+        `${AppRoute.Topics}${encodeURIComponent(slug)}`
+
+      expect(href)
+        .toEqual(hrefToBe)
+
+      expect(foundLink)
+        .toHaveTextContent(mockItems[index].title)
+    })
+  })
+
+
+  it('passes "null" as list', () => {
+    render(
+        <HistoryList
+          items={null as unknown as HistoryEntries}
+        />
+    )
+
+    expect(screen.queryByRole('heading'))
+      .not.toBeInTheDocument()
+  })
+})
--- utils/mocks.ts
+++ utils/mocks.ts
@@ -1,4 +1,5 @@
 import { datatype, lorem } from 'faker';
+import { HistoryEntries, HistoryEntry } from 'types/history';
 import { SearchTopic, SearchTopics } from 'types/search-tags';
 
 function makeMockSearchTopic(attributes?: Partial<SearchTopic>): SearchTopic {
@@ -13,4 +14,25 @@ export function makeMockSearchTopics(count: number): SearchTopics {
   return new Array(count).
     fill({}).
     map(() => makeMockSearchTopic());
+}
+
+function makeMockHistoryEntry(attributes?: Partial<HistoryEntry>): HistoryEntry {
+  const id = Math.random() < 0.5 ? datatype.uuid() : undefined;
+  const slug = Math.random() < 0.5 ? lorem.slug() : undefined;
+
+  return {
+    id: attributes?.id ?? id,
+    slug: attributes?.slug ?? slug,
+    title: attributes?.title ?? datatype.string(),
+  };
+}
+
+export function makeMockHistoryEntries(count: number): HistoryEntries {
+  if (count === 0) {
+    return [];
+  }
+
+  return new Array(count).
+    fill({}).
+    map(() => makeMockHistoryEntry());
 }
