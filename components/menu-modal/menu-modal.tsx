0de749d7d11b77938c0fb258cdbde6194e56c9fc
rrramble <rustam.arkharov@gmail.com>
2023-05-10 10:25:39 +0600

    test: add 'dataTestId?' prop into <MenuModal>

--- components/menu-modal/menu-modal.tsx
+++ components/menu-modal/menu-modal.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames'
 import styles from './menu-modal.module.scss'
 
 type MenuModalProps = {
-  children: ReactNode,
+  dataTestId?: string,
   className: string,
   isHidden: boolean,
   isFullHeight: boolean,
@@ -13,6 +13,7 @@ export default function MenuModal(props: MenuModalProps) {
   return (
     <div
       className={props.className}
+      data-testid={props.dataTestId}
     >
       <div
         className={classNames(styles.self, {
